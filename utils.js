cfd51a097fa3c60f46b22a0abfd73cb41667cc7b
wang_ln <wang_ln@Ctrip.com>
2020-02-05 17:09:04 +0800

    refactor(leetcoding):coding

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ const glob = require('glob');
 //取得相应的页面路径,因为之前的配置,所以是src文件夹下的pages文件夹
 const PAGE_PATH=[
     path.resolve(__dirname,"./src"),
-  ];
+];
   
   //多入口配置
   //通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件，如果该文件存在，那么就作为入口处理
@@ -19,7 +19,7 @@ exports.entries = ()=>{
       map[filename] = filePath
     });
     return map;
-  }
+}
   //提取出文件名
   function generateFileName(filePath){
     let filename = filePath.substring(filePath.lastIndexOf('\/')+1,filePath.lastIndexOf('.'))
